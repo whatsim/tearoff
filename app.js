a61a72d5dc715844e967ed478b1283a1afa01172
will ruby <me@willruby.com>
2013-09-03 15:49:32 -0700

    kind of working post expiration, fake data entry w/ redis

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express	= require('express'), app = express();
 var jade	= require('jade');
 var Q 		= require('q');
 var db		= require('redisDriver');
+var sha1	= require('sha1');
 
 // express conf
 
@@ -16,8 +17,8 @@ var serverRoot = require('path').dirname(require.main.filename);
 
 var fPageObj = {
 	text	:	'copy goes here',	// the message. markdown likely
-	expires	:	10,					// time the message is no longer accessible (only deleted if actually accessed after that time?)
-	loads	:	-1,					// number of allowed loads
+	expires	:	-1,					// time the message is no longer accessible (only deleted if actually accessed after that time?)
+	loads	:	10,					// number of allowed loads
 	visitors : [],					// hash of ip to remember them by
 	strict : true					// repeated loads from one user deplete the count
 }
@@ -65,17 +66,18 @@ function getPage(req,res){
 		.then(loaded, error);
 	
 	function loaded(data){
-		
-		if(data.strict) data.loads --;
-		else {
-			var ipHash = "ASDASDASDA";
+		if(data.strict){
+			data.loads --;
+		} else {
+			var ipHash = sha1(req.ip);
+			
 			if(data.vistors.indexOf(ipHash) < 0){
 				data.vistors.push(ipHash);
 				data.loads --;
 			}
 		}
-		
-		if(data.loads > 0){
+		console.log('hello');
+		if((1*data.loads) > 0){
 			
 			db.updatePage(req.params.page,data)
 				.then(success,error)	
@@ -85,9 +87,10 @@ function getPage(req,res){
 				.then(success,error)	
 		}
 	
-		function success(reply){
-			res.send(reply);
-		}
+	}
+	
+	function success(reply){
+		res.send(reply);
 	}
 	
 	function error(err){
@@ -106,5 +109,3 @@ function getPeek(req,res){
 		res.json(err);
 	}
 }
-
-// generate Random string of length

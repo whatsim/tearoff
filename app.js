57aeed64702490496b7ca07ba8a81f44dce6c051
will ruby <me@willruby.com>
2014-09-26 16:17:20 -0700

    add one additional view to account for the load success view

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function postPage(req,res){
 
 	var saveO = {
 		pageText : util.marked(req.body.pageText),
-		loads : req.body.loads,
+		loads : req.body.loads + 1,
 		strict : req.body.strict,
 		expires : time.setUTCDate(req.body.days+time.getUTCDate()),
 		visitors : []
